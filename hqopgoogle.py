4e4beaa288259db6859ee51de619c8c07b9ac16a
Kumar Dhruv <54675483+Technology-cell@users.noreply.github.com>
2021-05-01 15:43:54 +0530

    Add files via upload

---
+++ hqopgoogle.py
@@ -0,0 +1,129 @@
+from dhooks import Webhook
+from lomond import WebSocket
+import time
+import json
+from unidecode import unidecode
+import discord
+import requests
+
+def show_not_on():
+	url = "https://api-quiz.hype.space/shows/now"
+	response = requests.get(url=url).json()
+	if "broadcast" not in response or response["broadcast"] is None:
+		if "error" in response and response["error"] == "Auth not valid":
+			raise RuntimeError("Invalid Bearer")
+		else:
+			print("Game is not live")
+
+def show_active():
+	url = "https://api-quiz.hype.space/shows/now"
+	response = requests.get(url=url).json()
+	return response["active"]
+
+def connect_websocket(url : str,token : str,cbc = None):
+	hook = Webhook(url)
+	headers = {"Authorization": f"Bearer {token}"}
+	url = requests.get(url="https://api-quiz.hype.space/shows/now").json()["broadcast"]["socketUrl"].replace("https","wss")
+	websocket = WebSocket(url)
+	for header, value in headers.items():
+		websocket.add_header(str.encode(header), str.encode(value))
+	for msg in websocket.connect(ping_rate=5):
+		if msg.name == "text":
+			message = msg.text
+			data = json.loads(message)
+			if data["type"] == "question":
+				questio = data["question"]
+				qno = data["questionNumber"]
+				totalq = data["questionCount"]
+				question = str(questio).replace(" ","+")
+				answers = [unidecode(ans["text"]) for ans in data["answers"]]
+				answersid = [answer["answerId"] for answer in data["answers"]]
+				option1 = f"{answers[0]}"
+				option2 = f"{answers[1]}"
+				option3 = f"{answers[2]}"
+				op1 = option1.replace(" ","+")
+				op2 = option2.replace(" ","+")
+				op3 = option3.replace(" ","+")
+				url="https://www.google.com/search?q=" + question
+				embed = discord.Embed(title=f"**Question {qno} out of {totalq}**",description=f"**[{questio}]({url})\n\n[Search with options]({url}+{op1}+{op2}+{op3})**",color=000000)
+				#embed.add_field(name=f"**{questio}**",value=f"**[Search with options]({url}+{op1}+{op2}+{op3})**")
+				embed.add_field(name="**Option 1**",value=f"**[{option1}]({url}+{op1})**")
+				embed.add_field(name="**Option 2**",value=f"**[{option2}]({url}+{op2})**")
+				embed.add_field(name="**Option 3**",value=f"**[{option3}]({url}+{op3})**")
+				embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/835091231301304340/836795845282103296/IMG_20210428_081743.jpg")
+				hook.send(embed=embed)
+				#hook2.send(embed=embed)
+				#hook3.send(embed=embed)
+				search = requests.get(url=url)
+				searchop = requests.get(url=url + "+" + option1 + "+" + option2 + "+" + option3)
+				res = str(search.text)
+				resop = str(searchop.text)
+				count1 = res.count(option1)
+				count2 = res.count(option2)
+				count3 = res.count(option3)
+				countop1 = resop.count(option1)
+				countop2 = resop.count(option2)
+				countop3 = resop.count(option3)
+				find1 = res.find(option1)
+				find2 = res.find(option2)
+				find3 = res.find(option3)
+				embed = discord.Embed(title="**Google Results!**",url=url,description="",color=000000)
+				embed.add_field(name="**__Search without options__**",value=f"**Option 1 : {count1}\nOption 2 : {count2}\nOption 3 : {count3}**")
+				embed.add_field(name="**__Search with options__**",value=f"**Option 1 : {countop1}\nOption 2 : {countop2}\nOption 3 : {countop3}**")
+				embed.add_field(name="**__Special Search__**",value=f"**Option 1 : {find1}\nOption 2 : {find2}\nOption 3 : {find3}**")
+				embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/827177417092366396/834782681648595005/a7372eaaeafa289f28534ad39d96d517.gif")
+				embed.set_author(name="HQ Trivia",icon_url="https://cdn.discordapp.com/attachments/835091231301304340/836795845282103296/IMG_20210428_081743.jpg")
+				embed.set_footer(text="</> by Kumar Dhruv",icon_url="https://cdn.discordapp.com/attachments/827177417092366396/834785101153566720/IMG_20210130_095034.jpg")
+				hook.send(cbc)
+				hook.send(embed=embed)
+				#hook2.send(cbc2)
+				#hook2.send(embed=embed)
+				#hook3.send(cbc3)
+				#hook3.send(embed=embed)
+			if data["type"] == "answered":
+				name = data["username"]
+				answer = data["answerId"]
+				if name == "":
+					uname = "Marvel Owner"
+				if name == "":
+					uname = ""
+				if name == "":
+					uname = ""
+				if name == "":
+					uname = ""
+				if name == "":
+					uname = ""
+				if answer == answersid[0]:
+					embed = discord.Embed(title=f"**Marvel Friends**",description=f"**{uname} went option 1**",color=000000)
+					hook.send(embed=embed)
+				if answer == answersid[1]:
+					embed = discord.Embed(title=f"**Marvel Friends**",description=f"**{uname} went option 2**",color=000000)
+					hook.send(embed=embed)
+				if answer == answersid[2]:
+					embed = discord.Embed(title=f"**Marvel Friends**",description=f"**{uname} went option 3**",color=000000)
+					hook.send(embed=embed)
+			if data["type"] == "questionSummary":
+				correct = ""
+				for answer in data["answerCounts"]:
+					ans_str = unidecode(answer["answer"])
+					if answer["correct"]:
+						correct = ans_str
+				advancing = data["advancingPlayersCount"]
+				eliminated = data["eliminatedPlayersCount"]
+				print(f"Correct : {correct}\nAdvancing : {advancing}\nEliminated : {eliminated}")
+
+			if data["type"] == "gameSummary":
+				winners = data["numWinners"]
+				payout = str(data["winners"][0]["prize"])
+				print(f"Winners : {winners}\nPayout : {payout}")
+
+while True:
+	token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjI2ODg5NDU2LCJ1c2VybmFtZSI6ImNoYWRMYW0iLCJhdmF0YXJVcmwiOiJodHRwczovL2Nkbi5wcm9kLmh5cGUuc3BhY2UvZGEvZ29sZC5wbmciLCJ0b2tlbiI6bnVsbCwicm9sZXMiOltdLCJjbGllbnQiOiJpUGhvbmU4LDIiLCJndWVzdElkIjpudWxsLCJ2IjoxLCJpYXQiOjE2MTkxMTE0MTUsImV4cCI6MTYyNjg4NzQxNSwiaXNzIjoiaHlwZXF1aXovMSJ9.jmRVRfC3TFsNz8WgMdPPtWS0NBjdH_nTxVoqsBmwWBs"
+	#url = "https://discord.com/api/webhooks/835083147761811467/4ZVK4BxJ_zIxgtG2zUtWoCabc13tp4aP0KA-LMfeAzliTDesgQrgw6JMtxbdeC3fjY0G"
+	#url = "https://discord.com/api/webhooks/763403798999203880/z-gW0sSMuKwjXKS8SfX6Q3mDRhXFiVMZFVovd3E5L75Xri0aTu3sM8Q99GPFj0UJcFed"
+	url = "https://discord.com/api/webhooks/835764960344670228/B4F2PTg5FCNzyMhwudqtC3Oz0-vbvY6AMToIPm987IkBTWpO9keQ0CZMR1oqulNUI4He"
+	if show_active():
+		connect_websocket(url,token,"+mt")
+	else:
+		show_not_on()
+		time.sleep(30)
