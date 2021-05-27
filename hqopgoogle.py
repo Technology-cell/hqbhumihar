6f4dbedcaa9ff355cb30f69c3ffa8f165d998f84
Kumar Dhruv <54675483+Technology-cell@users.noreply.github.com>
2021-05-27 19:19:13 +0530

    Update hqopgoogle.py

--- hqopgoogle.py
+++ hqopgoogle.py
@@ -95,7 +95,7 @@ while True:
 	url = "https://discord.com/api/webhooks/841643870042849341/bjyT1dKqVchWdhay1G0hyVcUFr2YneFY3J7ydgOWDUHXLgVDjGkDLbNuoS9qVUFD5HEV"
 	url2 = "https://discord.com/api/webhooks/838271937888780318/jUoH7tbiyE6PZS-FMj34EBJM7CjY0S1NDgiipf_xd36kkbTMoc3ouQyIAMU2EJV6gH01"
 	url3 = "https://discord.com/api/webhooks/840991903162957864/w_poXWoDETQvt8c_nVoyIj_SW524h4iu9nZs1O0qe4mN8c-KxETLCAXX5ft9Ehf_gDDI"
-	url4 = ""
+	url4 = "https://discord.com/api/webhooks/847470950442205224/ZKbwurLcLQycy7r9ci_dpgs28yzuY2uOa1S75pH1BLcTjzwV4QYdqZ0ZAzkAeG5Ju9gN"
 	if show_active():
 		connect_websocket(url,url2,url3,url4,token)
 	else:
